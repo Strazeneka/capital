c6dd7b18a3f0d83ee2e9ef9b48f36dac1d68bfd8
elbahaouiimane <imane32321@gmail.com>
2024-04-09 01:57:28 +0000

    new

--- app/src/main/java/com/example/quizappli_elbahaoui/MainActivity.java
+++ app/src/main/java/com/example/quizappli_elbahaoui/MainActivity.java
@@ -3,23 +3,31 @@ package com.example.quizappli_elbahaoui;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText etLogin, etPassword;
     Button bLogin;
     TextView tvRegister;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,17 +37,25 @@ public class MainActivity extends AppCompatActivity {
         etPassword = (EditText) findViewById(R.id.etPassword);
         bLogin = (Button) findViewById(R.id.bLogin);
         tvRegister = (TextView) findViewById(R.id.tvRegister);
+        mAuth = FirebaseAuth.getInstance();
         //Step 3: Association de listeners
         bLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if (etLogin.getText().toString().equals("toto") && etPassword.getText().toString().equals("123")){
-                    startActivity(new Intent(MainActivity.this, Quiz1.class));
-                }
-                else {
+              //  if (etLogin.getText().toString().equals("toto") && etPassword.getText().toString().equals("123"))
+              //  {
+              //      startActivity(new Intent(MainActivity.this, Quiz1.class));
+               // }
+                String email = etLogin.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
                     Toast.makeText(getApplicationContext(),"Login or password incorrect !",Toast.LENGTH_SHORT).show();
+                    return;
                 }
+                signIn(email, password);
+                startActivity(new Intent(MainActivity.this, Quiz1.class));
+                // signIn(etLogin.getText().toString(),etPassword.getText().toString());
             }
         });
         tvRegister.setOnClickListener(new View.OnClickListener() {
@@ -50,4 +66,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(MainActivity.this, "Sign in success!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(MainActivity.this, Quiz1.class));
+
+                            // You can add code here to navigate to another activity
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Sign in failed. Please try again.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
+
 }
--- app/src/main/java/com/example/quizappli_elbahaoui/Register.java
+++ app/src/main/java/com/example/quizappli_elbahaoui/Register.java
@@ -3,6 +3,7 @@ package com.example.quizappli_elbahaoui;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,12 +16,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class Register extends AppCompatActivity {
 
     EditText etMail, etPassword, etPassword1;
     Button bRegister;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +35,7 @@ public class Register extends AppCompatActivity {
         etPassword=(EditText) findViewById(R.id.etPassword);
         etPassword1=(EditText)findViewById(R.id.etPassword1);
         bRegister=(Button)findViewById(R.id.bRegister);
+        mAuth = FirebaseAuth.getInstance();
         bRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,14 +62,37 @@ public class Register extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(),"Please enter correct password",Toast.LENGTH_SHORT).show();
                     return;
                 }
+                signUpWithEmailAndPassword(mail, password);
+
 
                 //Commen.login=Mail;
                 //Commen.password=password;
                 Toast.makeText(getApplicationContext(),"Registration Successful!  inscription réussi! التسجيل ناجح! 註冊成功  ",Toast.LENGTH_LONG).show();
                 startActivity(new Intent(Register.this,MainActivity.class));
+
                 finish();
             }
         });
     }
+    private void signUpWithEmailAndPassword(String mail, String password) {
+        mAuth.createUserWithEmailAndPassword(mail, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // L'inscription a réussi, l'utilisateur est créé et connecté.
+                            Toast.makeText(Register.this, "Inscription réussie.", Toast.LENGTH_SHORT).show();
+                            // Vous pouvez rediriger l'utilisateur vers une autre activité ici
+                        } else {
+                            // L'inscription a échoué.
+                            Toast.makeText(Register.this, "Inscription échouée. Veuillez réessayer.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 }
+
+
+
+
 
